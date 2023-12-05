319d2460ff60b9e1418196da601d112533e6b481
emiliano jossie <jossie@tennet.id>
2023-12-05 20:59:51 +0700

    ya

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,25 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function login(Request $request)
+    {   
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            session(["token" => auth()->user()->createToken($request->email)->plainTextToken]);
+            return redirect()->route('home');
+        }
+        else{
+            $validator->getMessageBag()->add('email', 'Username atau Password salah');
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+    }
+    protected function authenticated(Request $request, $user)
+    {
+        dd('halo');
     }
 }
