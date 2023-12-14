13f5055ea1df08fb90d6b8a7151c777886787978
Aldi Dharmawan <aldidharmawan700@gmail.com>
2023-12-14 11:11:04 +0700

    update views blade

--- resources/views/pemerintahan-posyandu/edit.blade.php
+++ resources/views/pemerintahan-posyandu/edit.blade.php
@@ -1,75 +1,133 @@
 @extends('layouts.app')
-@section('master-title','Posyandu/')
-@section('page-title','Edit')
+@section('master-title', 'Posyandu/')
+@section('page-title', 'Edit')
 @section('contents')
-    <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
-                <form action="{{ route('pemerintahan-desa.update',$data->id) }}" method="post" enctype="multipart/form-data">
-                    @method('PUT')    
-                    @csrf
-                    <div class="card">
-                        <div class="card-body">
+    <!-- DataTables -->
+    <link href="{{ asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet"
+        type="text/css" />
+
+    <!-- Responsive datatable examples -->
+    <link href="{{ asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet"
+        type="text/css" />
+
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <main>
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+
+            <form action="{{ route('pemerintahan-desa.update', $data->id) }}" method="post" enctype="multipart/form-data">
+                @method('PUT')
+                @csrf
+                <div class="card">
+                    <div class="card-body">
 
 
-                            <h5 class="text-center data_diri">Data Diri</h5>
+                        <h5 class="text-center data_diri">Data Diri</h5>
 
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-sm-4">
-                                        <label for="profile" class="col-form-label">Profile</label>
-                                        <input type="file" class="form-control" id="profile"
-                                            name="profile" placeholder="">
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <label for="name" class="col-form-label">Nama Lengkap</label>
-                                        <input type="text" class="form-control" id="nama"
-                                            name="nama" value="{{ $data->nama }}">
-                                    </div>
-
-                                    <div class="col-sm-4">
-                                        <label for="jabatan" class="col-form-label">Jabatan</label>
-                                        <input type="text" class="form-control" id="jabatan"
-                                            name="jabatan" value="{{ $data->jabatan }}">
-                                    </div>
-
-                                    <div class="col-sm-4">
-                                        <label for="tmpt_lahir" class="col-form-label">Tempat Lahir</label>
-                                        <input type="text" class="form-control" id="tmpt_lahir"
-                                            name="tmpt_lahir" value="{{ $data->tmpt_lahir}}">
-                                    </div>
-
-                                    <div class="col-sm-4">
-                                        <label for="tgl_lahir" class="col-form-label">Tanggal Lahir</label>
-                                        <input type="date" class="form-control" id="tgl_lahir"
-                                            name="tgl_lahir" value="{{ $data->tgl_lahir }}">
-                                    </div>
-
-                                    <div class="col-sm-4">
-                                        <label for="jns_kelamin" class="col-form-label">Jenis Kelamin</label>
-                                        <select class="form-control" id="jns_kelamin" name="jns_kelamin">
-                                            <option value="1" @if($data->jns_kelamin == '1') selected @endif>Laki-Laki</option>
-                                            <option value="2" @if($data->jns_kelamin == '2') selected @endif>Perempuan</option>
-                                        </select>
-                                    </div>
-
-                                    <div class="col">
-                                        <label for="alamat" class="col-form-label">Alamat</label>
-                                        <textarea rows="5" type="text" class="form-control" id="alamat" name="alamat" >{{ $data->alamat }}</textarea>
-                                    </div>
-
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <label for="profile" class="col-form-label">Profile</label>
+                                    <input type="file" class="form-control" id="profile" name="profile" placeholder="">
                                 </div>
-                            </div>
+                                <div class="col-sm-4">
+                                    <label for="name" class="col-form-label">Nama Lengkap</label>
+                                    <input type="text" class="form-control" id="nama" name="nama"
+                                        value="{{ $data->nama }}">
+                                </div>
 
-                            <div class="d-flex flex-row-reverse">
-                                <button type="submit" class="btn btn-primary ml-3">Simpan</button>
-                                <a href="{{ route('pemerintahan-desa.index') }}" class="btn btn-danger">Batal</a>
-                            </div>
+                                <div class="col-sm-4">
+                                    <label for="jabatan" class="col-form-label">Jabatan</label>
+                                    <input type="text" class="form-control" id="jabatan" name="jabatan"
+                                        value="{{ $data->jabatan }}">
+                                </div>
 
-    
-    <!-- /.card-body -->
-    </form>
-    <!-- /.row (main row) -->
-</div><!-- /.container-fluid -->
+                                <div class="col-sm-4">
+                                    <label for="tmpt_lahir" class="col-form-label">Tempat Lahir</label>
+                                    <input type="text" class="form-control" id="tmpt_lahir" name="tmpt_lahir"
+                                        value="{{ $data->tmpt_lahir }}">
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <label for="tgl_lahir" class="col-form-label">Tanggal Lahir</label>
+                                    <input type="date" class="form-control" id="tgl_lahir" name="tgl_lahir"
+                                        value="{{ $data->tgl_lahir }}">
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <label for="jns_kelamin" class="col-form-label">Jenis Kelamin</label>
+                                    <select class="form-control" id="jns_kelamin" name="jns_kelamin">
+                                        <option value="1" @if ($data->jns_kelamin == '1') selected @endif>Laki-Laki
+                                        </option>
+                                        <option value="2" @if ($data->jns_kelamin == '2') selected @endif>Perempuan
+                                        </option>
+                                    </select>
+                                </div>
+
+                                <div class="col">
+                                    <label for="alamat" class="col-form-label">Alamat</label>
+                                    <textarea rows="5" type="text" class="form-control" id="alamat" name="alamat">{{ $data->alamat }}</textarea>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="d-flex flex-row-reverse">
+                            <button type="submit" class="btn btn-primary ml-3">Simpan</button>
+                            <a href="{{ route('pemerintahan-desa.index') }}" class="btn btn-danger">Batal</a>
+                        </div>
+
+
+                        <!-- /.card-body -->
+            </form>
+            <!-- /.row (main row) -->
+        </div><!-- /.container-fluid -->
+    </main>
+
+    <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+
+    <!-- Required datatable js -->
+    <script src="{{ asset('assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <!-- Buttons examples -->
+    <script src="{{ asset('assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/pdfmake/build/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/pdfmake/build/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
+    <!-- Responsive examples -->
+    <script src="{{ asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+
+    <!-- Datatable init js -->
+    <script src="{{ asset('assets/js/pages/datatables.init.js') }}"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/fontawesome.js"
+        integrity="sha384-dPBGbj4Uoy1OOpM4+aRGfAOc0W37JkROT+3uynUgTHZCHZNMHfGXsmmvYTffZjYO" crossorigin="anonymous">
+    </script>
+
 
 @endsection
--- resources/views/penduduk/index.blade.php
+++ resources/views/penduduk/index.blade.php
@@ -1,12 +1,7 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <title>Data Tables | Qovex - Responsive Bootstrap 4 Admin Dashboard</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+@extends('layouts.app')
+@section('master-title', 'Dashboard/')
+@section('page-title', 'Sahbandar')
+@section('contents')
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
@@ -48,35 +43,14 @@
                 <div class="page-content">
 
                     <!-- start page title -->
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="page-title-box d-flex align-items-center justify-content-between">
-                                <h4 class="page-title mb-0 font-size-18">Data Penduduk</h4>
-
-                                <div class="page-title-right">
-                                    <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                                        <li class="breadcrumb-item active">Data Penduduk</li>
-                                    </ol>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
+              
                     <!-- end page title -->
 
                     <div class="row">
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-body">
-                                    <div class="d-flex flex-row">
-                                        <button type="button" class="btn btn-primary button_data_tambah">Tambah Data
-                                            Penduduk</button>
-                                        <button type="button" class="btn btn-secondary  button_data_import">Import Data
-                                            Penduduk</button>
-                                        <button type="button" class="btn btn-success  button_data_export">Eksport Data
-                                            Penduduk</button>
-                                    </div>
+                                   
                                     <table id="datatable" class="table table-bordered dt-responsive nowrap"
                                         style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                         <thead>
