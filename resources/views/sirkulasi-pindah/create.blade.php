860ca4e6e49893d8e6a10800f1d96ed7bd61323f
solehiskandar <132457694+solehiskandar@users.noreply.github.com>
2023-12-26 20:40:02 +0700

    try

--- resources/views/sirkulasi-pindah/create.blade.php
+++ resources/views/sirkulasi-pindah/create.blade.php
@@ -32,7 +32,7 @@
 
                 <!-- Small boxes (Stat box) -->
 
-                <form action="{{ route('sirkulasi-pindah.store') }}" method="POST" enctype="multipart/form-data">
+                {{-- <form action="{{ route('sirkulasi-pindah.store') }}" method="POST" enctype="multipart/form-data"> --}}
                     @csrf
                     <div class="card">
                         <div class="card-body">
@@ -73,12 +73,12 @@
 
                             <div class="d-flex flex-row-reverse">
                                 <button type="submit" class="btn btn-primary ml-3">Simpan</button>
-                                <a href="{{ route('sirkulasi-pindah.index') }}" class="btn btn-danger">Batal</a>
+                                {{-- <a href="{{ route('sirkulasi-pindah.index') }}" class="btn btn-danger">Batal</a> --}}
                             </div>
 
 
                             <!-- /.card-body -->
-                </form>
+                {{-- </form> --}}
                 <!-- /.row (main row) -->
             </div><!-- /.container-fluid -->
         </section>
