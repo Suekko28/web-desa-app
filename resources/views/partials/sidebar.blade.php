4c2575613023dae97a8f6ec707b8d37f4a76ee55
Aldi Dharmawan <aldidharmawan700@gmail.com>
2024-01-15 09:34:07 +0700

    Update sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -99,9 +99,7 @@
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href="{{ route('lpj-barangjasa.index') }}">Barang dan Jasa</a></li>
-                    </ul>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="">Tim Pemeriksa</a></li>
+                        <li><a href="#">Tim Pemeriksa</a></li>
                     </ul>
                 </li>
 
